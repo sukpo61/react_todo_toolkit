1105faecc71b82e7d8b05789273fc96479302a82
GH <sukpo6010@gmail.com>
2022-12-12 19:25:54 +0900

    툴킷적용완료

--- src/redux/modules/todos.js
+++ src/redux/modules/todos.js
@@ -1,22 +1,52 @@
-// Action value
-const ADD_TODO = "ADD_TODO";
-const DELETE_TODO = "DELETE_TODO";
-const TOGGLE_TODO = "TOGGLE_TODO";
+import { createSlice } from "@reduxjs/toolkit";
 
-// Action Creator
-export const addTodo = (payload) => {
-  return { type: ADD_TODO, payload };
-};
+// // Action value
+// const ADD_TODO = "ADD_TODO";
+// const DELETE_TODO = "DELETE_TODO";
+// const TOGGLE_TODO = "TOGGLE_TODO";
 
-export const deleteTodo = (payload) => {
-  return { type: DELETE_TODO, payload };
-};
+// // Action Creator
+// export const addTodo = (payload) => {
+//   return { type: ADD_TODO, payload };
+// };
 
-export const toggleTodo = (payload) => {
-  return { type: TOGGLE_TODO, payload };
-};
+// export const deleteTodo = (payload) => {
+//   return { type: DELETE_TODO, payload };
+// };
+
+// export const toggleTodo = (payload) => {
+//   return { type: TOGGLE_TODO, payload };
+// };
 
 // initial State
+
+// // Reducer
+// const todos = (state = initialState, action) => {
+//   switch (action.type) {
+//     case ADD_TODO:
+//       return {
+//         ...state,
+//         todos: [...state.todos, action.payload],
+//       };
+//     case DELETE_TODO:
+//       return {
+//         todos: state.todos.filter((todos) => todos.id !== action.payload),
+//       };
+//     case TOGGLE_TODO:
+//       let Todolist = state.todos.slice();
+//       Todolist.find((e) => e.id === action.payload).isDone = !Todolist.find(
+//         (e) => e.id === action.payload
+//       ).isDone;
+
+//       return {
+//         todos: Todolist,
+//       };
+
+//     default:
+//       return state;
+//   }
+// };
+
 const initialState = {
   todos: [
     {
@@ -34,31 +64,27 @@ const initialState = {
   ],
 };
 
-// Reducer
-const todos = (state = initialState, action) => {
-  switch (action.type) {
-    case ADD_TODO:
-      return {
-        ...state,
-        todos: [...state.todos, action.payload],
-      };
-    case DELETE_TODO:
-      return {
-        todos: state.todos.filter((todos) => todos.id !== action.payload),
-      };
-    case TOGGLE_TODO:
+const todosSlice = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      state.todos = [...state.todos, action.payload];
+    },
+
+    deleteTodo: (state, action) => {
+      state.todos = state.todos.filter((todos) => todos.id !== action.payload);
+    },
+    toggleTodo: (state, action) => {
       let Todolist = state.todos.slice();
       Todolist.find((e) => e.id === action.payload).isDone = !Todolist.find(
         (e) => e.id === action.payload
       ).isDone;
+      state.todos = Todolist;
+    },
+  },
+});
 
-      return {
-        todos: Todolist,
-      };
-
-    default:
-      return state;
-  }
-};
-
-export default todos;
+export const { addTodo, deleteTodo, toggleTodo } = todosSlice.actions;
+// reducer 는 configStore에 등록하기 위해 export default 합니다.
+export default todosSlice.reducer;
